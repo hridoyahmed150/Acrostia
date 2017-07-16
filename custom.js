f455522ea6d99e8c457b3521cec3ecd19ab19b64
hridoy ahmed <hridoyahmed150@gmail.com>
2017-07-16 06:22:12 +0600

    this is new project

---
+++ custom.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+  	items:1,
+  	navText:['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>'],
+  	nav:true,
+  	loop:true,
+  	autoplay:true,
+  });
+});
+
+
+$(document).ready(function(){
+	var mixer = mixitup('.portfolio');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
