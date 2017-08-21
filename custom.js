801e46f0d16cf882c451ac978300a30fb1c7796f
hridoy ahmed <hridoyahmed150@gmail.com>
2017-08-21 23:25:52 +0600

    update in 8/21/17

--- custom.js
+++ custom.js
@@ -13,7 +13,35 @@ $(document).ready(function(){
 	var mixer = mixitup('.portfolio');
 });
 
+// $(function () {
+// 	var HomeOffset = $('#Home').offset().top;
+// 	var ServiceOffset = $('#Service').offset().top;
+// 	var AboutOffset = $('#About').offset().top;
+// 	var WorkOffset = $('#Work').offset().top;
+// 	var contactOffset = $('#contact').offset().top;
 
+// 	$(document).on('scroll',function(){
+
+// 		var scrollTop = $(document).scrollTop;
+// 		var activeLi;
+
+// 		if (scrollTop<ServiceOffset) {
+// 			activeLi = $('.menue>ul>li:nth-child(1)');
+// 		}
+// 		else if (scrollTop<AboutOffset) {
+// 			activeLi = $('.menue>ul>li:nth-child(2)');
+// 		}
+// 		else if (scrollTop<WorkOffset) {
+// 			activeLi = $('.menue>ul>li:nth-child(3)');
+// 		}
+// 		else
+// 		{
+// 			activeLi = $('.menue>ul>li:nth-child(4)');
+// 		}
+// 		activeLi.addClass('active'); 
+// 		$('.menue>ul>li').not(activeLi).removeClass('active');
+// 	});
+// });
 
 
 
